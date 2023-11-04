8b4cc63737a10ae9dbee8a4acc911e40d2bd5a1a
ghostb0x <ghostb0x.github@gmail.com>
2023-11-04 15:02:26 -0700

    Add details to the StartButton component for the workout preview details

--- src/components/StartButton/StartButton.js
+++ src/components/StartButton/StartButton.js
@@ -7,37 +7,28 @@ import { focusAreas } from '@/data/focusAreas';
 import { sessionHistory } from '@/data/sessions';
 
 function StartButton({ selectedFocusAreas }) {
-  // let today = moment().format('l');
-  // let startTime = moment().format('LT');
-  // focusAreas = [
-  //   {
-  //     id: 1,
-  //     name: 'Legs',
-  //     heavyMoves: [],
-  //   },
-  //   {
+  let today = moment().format('l');
+  let startTime = moment().format('LT');
 
   let todaysHeavyMoves = [];
-  if (selectedFocusAreas.length > 0) {
-
+  if (selectedFocusAreas.length) {
     selectedFocusAreas.forEach((selectedArea) => {
       const foundAreas = focusAreas.filter(
         (area) => area.name === selectedArea
-        );
-        foundAreas.forEach(({ heavyMoves }) =>
-        todaysHeavyMoves.push(heavyMoves)
-        );
+      );
+      foundAreas.forEach(({ heavyMoves }) => {
+        if (heavyMoves.length) {
+          heavyMoves.forEach((heavyMove) => todaysHeavyMoves.push(heavyMove));
+        }
       });
-      
-    }
+    });
+  }
   const newSession = {
-    // date: today,
-    // startTime: startTime,
-    date: '',
-    startTime: '',
+    date: today,
+    startTime: startTime,
     endTime: '',
     focusAreas: selectedFocusAreas,
-    hiitDuration: 30,
+    hiitDuration: 20,
     heavyMoves: todaysHeavyMoves,
     difficulty: 7,
   };
@@ -49,8 +40,13 @@ function StartButton({ selectedFocusAreas }) {
   return (
     <div>
       <p>The Plan: </p>
-      <br/>
-      <p>{todaysHeavyMoves}</p>
+      <br />
+      <p>
+        Today: {today} @ {startTime}
+      </p>
+      <p>Workout:</p>
+      <p>Hiit Warmup</p>
+      <p>and some of these: {todaysHeavyMoves.join(', ')}</p>
       <Button>Start Workout</Button>
     </div>
   );

5fae1f118c3a7731630c1e4445c3aa466d6880a5
ghostb0x <ghostb0x.github@gmail.com>
2023-12-31 14:56:24 -0800

    Re-installed radix primatives as dependency due to odd error - seemed to be installed locally, but not listed in my dev dependencies. Also, updated code in DeleteDialog.tsx component to use styled-components syntax instead of stitches (came with Radix component) to avoid additional package dependency

--- src/components/_Shared/DeleteDialog/DeleteDialog.tsx
+++ src/components/_Shared/DeleteDialog/DeleteDialog.tsx
@@ -1,14 +1,12 @@
 'use client';
 import React from 'react';
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import { styled, keyframes } from '@stitches/react';
+import styled, { keyframes } from 'styled-components';
 
 interface IDeleteDialogProps {
   children: React.ReactNode;
   confirmFunction: () => void;
 }
-
-// e?: React.MouseEvent<HTMLButtonElement, MouseEvent>
 
 function DeleteDialog({
   children,
@@ -24,18 +22,18 @@ function DeleteDialog({
           <AlertDialogDescription>
             This action cannot be undone.
           </AlertDialogDescription>
-          <Flex css={{ justifyContent: 'flex-end' }}>
+          <Flex>
             <AlertDialog.Cancel asChild>
               <Button
-                variant="blue"
-                css={{ marginRight: 25 }}
+                color="blue"
+                style={{ marginRight: 25 }}
               >
                 Cancel
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action asChild>
               <Button
-                variant="red"
+                color="red"
                 onClick={confirmFunction}
               >
                 Yes, delete item
@@ -48,89 +46,84 @@ function DeleteDialog({
   );
 }
 
-const overlayShow = keyframes({
-  '0%': { opacity: 0 },
-  '100%': { opacity: 1 },
-});
+const overlayShow = keyframes`
+  0% { opacity: 0 }
+  100% { opacity: 1 }
+`;
 
-const contentShow = keyframes({
-  '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
-  '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
-});
+const contentShow = keyframes`
+  0% { opacity: 0; transform: 'translate(-50%, -48%) scale(.96)' }
+  100% { opacity: 1; transform: 'translate(-50%, -50%) scale(1)' }
+`;
 
-const AlertDialogOverlay = styled(AlertDialog.Overlay, {
-  backgroundColor: 'hsla(360, 100%, 100%, 0.49)',
-  position: 'fixed',
-  inset: 0,
-  animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
-});
+const AlertDialogOverlay = styled(AlertDialog.Overlay)`
+  background-color: hsla(360, 100%, 100%, 0.49);
+  position: fixed;
+  inset: 0;
+  animation: ${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
+`;
 
-const AlertDialogContent = styled(AlertDialog.Content, {
-  backgroundColor: 'black',
-  borderRadius: 6,
-  boxShadow:
-    'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '90vw',
-  maxWidth: '500px',
-  maxHeight: '85vh',
-  padding: 25,
-  animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
+const AlertDialogContent = styled(AlertDialog.Content)`
+  background-color: black;
+  border-radius: 6px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 500px;
+  max-height: 400px;
+  height: auto;
+  padding: 25px;
+  animation: ${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
 
-  '&:focus': { outline: 'none' },
-});
+  &:focus {
+    outline: none;
+  }
+`;
 
-const AlertDialogTitle = styled(AlertDialog.Title, {
-  margin: 0,
-  color: 'white',
-  fontSize: 17,
-  fontWeight: 500,
-});
+const AlertDialogTitle = styled(AlertDialog.Title)`
+  margin: 0;
+  color: white;
+  font-size: 17px;
+  font-weight: 500px;
+`;
 
-const AlertDialogDescription = styled(AlertDialog.Description, {
-  marginBottom: 20,
-  color: 'white',
-  fontSize: 15,
-  lineHeight: 1.5,
-});
+const AlertDialogDescription = styled(AlertDialog.Description)`
+  margin-bottom: 20px;
+  color: white;
+  font-size: 15px;
+  line-height: 1.5;
+`;
 
-const Flex = styled('div', { display: 'flex' });
+const Flex = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  
+`;
 
-const Button = styled('button', {
-  all: 'unset',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: 4,
-  padding: '0 15px',
-  fontSize: 15,
-  lineHeight: 1,
-  fontWeight: 500,
-  height: 35,
+const Button = styled.button`
+  all: unset;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  padding: 0 15px;
+  font-size: 15px;
+  line-height: 1;
+  font-weight: 500;
+  height: 35px;
+  background-color: ${props => props.color === 'red' ? 'red' : 'var(--color-primary)'};
+  color: white;
 
-  variants: {
-    variant: {
-      red: {
-        backgroundColor: 'red',
-        color: 'white',
-        '&:hover': { backgroundColor: 'maroon' },
-        '&:focus': { boxShadow: `0 0 0 2px red` },
-      },
-      blue: {
-        backgroundColor: 'var(--color-primary)',
-        color: 'white',
-        '&:hover': { backgroundColor: '#065dbb' },
-        '&:focus': { boxShadow: `0 0 0 2px #065dbb` },
-      },
-    },
-  },
+  &:hover {
+    background-color: ${props => props.color === 'red' ? 'maroon' : '#065dbb'};
+  }
 
-  defaultVariants: {
-    variant: 'blue',
-  },
-});
+  &:focus {
+    box-shadow: 0 0 0 2px ${props => props.color === 'red' ? 'maroon' : '#065dbb'};
+  }
+`;
 
 export default DeleteDialog;
